1582a9d3a091f7359798f4bea166a4a77230c8e2
Raquel Montoliu Balaguer <rmonbal1@etsinf.upv.es>
2024-11-28 17:45:56 +0100

    Añadido el MakeFile y el código de test de la clase TableEntry

---
+++ Makefile
@@ -0,0 +1,9 @@
+bin/testTableEntry: testTableEntry.cpp TableEntry.h
+	mkdir -p bin
+	g++ -o bin/testTableEntry testTableEntry.cpp
+clean: 
+	rm -rf *.o *.gch bin
+
+bin/testHashTable: testHashTable.cpp Dict.h HashTable.h TableEntry.h  # modificar si hace falta
+	mkdir -p bin
+	g++ -o bin/testHashTable testHashTable.cpp
---
+++ TableEntry.h
@@ -0,0 +1,31 @@
+#ifndef TABLEENTRY_H
+#define TABLEENTRY_H
+#include <string>
+#include <ostream>
+
+template <typename V>
+class TableEntry {
+	public: 
+	// Atributos
+	std::string key;
+	V value;
+	
+	//Métodos constructores
+	TableEntry (std::string key, V value) : key(key), value(value) {} // Inicializamos todos los valores
+	TableEntry (std::string key): key(key), value(V()) {}
+	TableEntry() : key (""), value (V()) {}
+
+	//Métodos sobrecargadores operacionales:
+	friend bool operator == (const TableEntry<V> &te1, const TableEntry<V> &te2){ 
+		return te1.key == te2.key;
+	}
+	friend bool operator != (const TableEntry<V> &te1, const TableEntry<V> &te2){
+		return te1.key != te2.key;
+	}
+
+	friend std::ostream& operator << (std::ostream &out, const TableEntry<V> &te){
+		out << "(Clave: " << te.key << ")" << " (Valor: " << te.value << ")";
+		return out;
+	}
+};
+#endif
